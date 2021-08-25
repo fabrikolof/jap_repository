003cb4eefb9a4fc0add5cf2492adad9bfa7467d1
fabriciobarrios <fabribcom@gmail.com>
2021-08-25 07:53:52 -0300

    google 11

--- js/signOut.js
+++ js/signOut.js
@@ -7,10 +7,3 @@ function cerrarSesion() {
   
 }
 
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-      window.location.assign("login.html");
-    });
-  }
