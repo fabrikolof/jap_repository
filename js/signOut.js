51e64898e34580af84b6a6027d276c1c4baacfab
fabriciobarrios <fabribcom@gmail.com>
2021-08-26 10:39:17 -0300

    google15

--- js/signOut.js
+++ js/signOut.js
@@ -1,10 +1,35 @@
 document.addEventListener("DOMContentLoaded", function (e) {
 
     document.getElementById("cerrarSesion").addEventListener("click", function (e) {
-  
-      localStorage.removeItem("User");
-      window.location = "login.html";
-  
+        signOut()
+        onLoad()
     });
-  
-  });
+
+});
+
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        location.href = "index.html";
+    });
+    localStorage.clear();
+    window.location = "index.html";
+}
+
+function onLoad() {
+    gapi.load("auth2", function () {
+        gapi.auth2.init();
+    });
+    /*
+        let bienvenidaDOM = document.getElementById("bienvenidaDOM");
+        let articlesOnCart = document.getElementById("articlesOnCart");
+        // let articlesOnCartLocalStorage = JSON.parse(localStorage.cantArt);
+        let userName = JSON.parse(localStorage.User);
+        let avatar = document.getElementById("avatar");
+    
+        bienvenidaDOM.innerHTML = `<strong>${userName.email}</strong>`;
+        avatar.style.backgroundImage = `url('${userName.avatar}')`;
+        // articlesOnCart.innerHTML = articlesOnCartLocalStorage;
+    */
+}
